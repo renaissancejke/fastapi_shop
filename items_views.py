3d0af7220157719de865cb62e7ab670c8963c62e
renaissance <rkrasnoshchekov@gmail.com>
2025-01-23 13:53:02 +0300

    first commit

---
+++ items_views.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Path
+
+
+router = APIRouter(prefix="/items", tags=["Items"])
+
+
+@router.get("/")
+def list_items():
+    return [
+        "Item1",
+        "Item2",
+        "Item3",
+    ]
+
+
+@router.get("/items/latest/")
+def get_latest_item():
+    return {"item": {"id": "0", "name": "latest"}}
+
+
+@router.get("/{item_id}")
+def get_item_by_id(item_id: Annotated[int, Path(ge=1, le=1_000_000)]):
+    return {"item": {"id": item_id}}
---
+++ main.py
@@ -0,0 +1,48 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+import uvicorn
+
+from core.models import Base, db_helper
+from items_views import router as items_router
+from users.views import router as users_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(items_router)
+app.include_router(users_router)
+
+
+@app.get("/")
+def hello_index():
+    return {
+        "message": "Hello index!",
+    }
+
+
+@app.get("/hello/")
+def hello(name: str = "World"):
+    name = name.strip().title()
+    return {"message": f"Hello {name}!"}
+
+
+@app.get("/calc/add/")
+def add(a: int, b: int):
+    return {
+        "a": a,
+        "b": b,
+        "result": a + b,
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
